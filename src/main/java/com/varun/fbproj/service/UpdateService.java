f18b9c1896d7a685c19deacb1ee0574d90db339d
varunjaiswal1891 <Varun.Jaiswal@iiitb.org>
2017-04-02 16:09:25 +0530

    event done kandoi 2 april

--- src/main/java/com/varun/fbproj/service/UpdateService.java
+++ src/main/java/com/varun/fbproj/service/UpdateService.java
@@ -1,6 +1,5 @@
 package com.varun.fbproj.service;
 
-import java.sql.PreparedStatement;
 
 import com.varun.fbproj.model.User;
 
@@ -8,6 +7,31 @@ public class UpdateService {
 
 	public static boolean UpdateUserService(User u1)
 	{
+		 int count=0;
+			StringBuilder sb= new StringBuilder("UPDATE User SET ");
+			if(u1.getMob_no().length()>0)
+			{	
+				sb.append("mob_no='"+u1.getMob_no()+ "',"); 
+		        count++;
+			}
+			if(u1.getCollege().length()>0)
+			{	sb.append("college='"+u1.getCollege()+ "',");  count++; }
+			if(u1.getPlaceOfWork().length()>0)
+			{	sb.append("placeOfWork='"+u1.getPlaceOfWork()+ "',");  count++;  }
+			if(u1.getHometown().length()>0)
+			{	sb.append("hometown='"+u1.getHometown()+ "',"); count++;}
+			if(u1.getCityOfWork().length()>0)
+			{	sb.append("cityOfWork='"+u1.getCityOfWork()+ "',"); count++;}
+			if(u1.getHighschool().length()>0)
+			{	sb.append("highschool='"+u1.getHighschool()+ "',"); count++;}
+		
+			sb.append("where emailID='"+u1.getEmailID()+"'");
+		    if(count>0){
+			   int index= sb.lastIndexOf(",");
+		       sb.deleteCharAt(index);
+		    }
+			String str= sb.toString();
+			System.out.println(str);
 
         try 
         {
@@ -18,9 +42,9 @@ public class UpdateService {
               	check=connect.start();
               	System.out.println("trying connection in update");
               }
-            String query = "UPDATE User SET "
+           /* String query = "UPDATE User SET "
             		+ "mob_no=?,college=?,placeOfWork=?,"
-            		+ "hometown=?,cityOfWork=?,highschool=? where emailID = ?";
+            		+ "hometown=?,cityOfWork=?,highschool=? , bestplace=? where emailID = ?";
            
             PreparedStatement ps = connect.con.prepareStatement(query);
             //ResultSet rs = stmt.getGeneratedKeys();
@@ -34,9 +58,13 @@ public class UpdateService {
 			ps.setString(4, u1.getHometown());
 			ps.setString(5,u1.getCityOfWork());
 			ps.setString(6, u1.getHighschool());
-			ps.setString(7,u1.getEmailID());
+			ps.setString(7, u1.getBestplace());
+			ps.setString(8,u1.getEmailID());
 			
-			ps.executeUpdate();
+			ps.executeUpdate();*/
+              if(count>0)
+                  connect.con.createStatement().executeUpdate(str);  
+              
 			check=connect.stop();
             return true;
         } 
